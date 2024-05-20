dfadc014a9ad961c03e560e772b63736dad6a0e9
geeteshkumbalkar28 <geeteshkumbalkar@gmail.com>
2024-05-21 03:03:48 +0530

    Added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import eslint from 'vite-plugin-eslint'
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+
 export default defineConfig({
-  plugins: [react(),eslint()],
-})
+  plugins: [
+    react(),
+    eslint(),
+  ],
+  build: {
+    rollupOptions: {
+      external: ['@material-ui/core/utils'],
+    },
+  },
+});
+
